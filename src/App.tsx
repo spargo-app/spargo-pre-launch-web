0b97333f0d88c8f3e560a2ac22d9c07eea7db6a3
omur <dodomrcn@gmail.com>
2023-01-04 19:21:36 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spargo</h1>
+      <hr style={{ width: "14rem" }} />
+      <p className="read-the-docs">Coming Soon...</p>
+    </div>
+  );
+}
+
+export default App;
